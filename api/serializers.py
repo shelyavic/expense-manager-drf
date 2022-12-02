b9dc97123078834358975ea75c6b0a775866b366
Ilya <ilya.shelepov2000@gmail.com>
2022-12-02 14:57:26 +0300

    Add api.serializers

---
+++ api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from api.models import Category, Transaction
+
+class LowercaseCharField(serializers.CharField):
+    def to_internal_value(self, data):
+        value = super().to_internal_value(data)
+        return value.lower()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    name = LowercaseCharField(max_length=255)
+    
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+        
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+        extra_kwargs = {"owner": {"read_only": True}}
