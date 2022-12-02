03109015efd55c65216cc1cdda243cc77359b066
Ilya <ilya.shelepov2000@gmail.com>
2022-12-02 14:58:24 +0300

    Add urls

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework_nested.routers import NestedSimpleRouter
+
+from api import views
+from users.urls import router as users_router
+
+
+router = NestedSimpleRouter(users_router, "users", lookup="user")
+router.register("categories", views.CategoryViewSet, basename="category")
+router.register("transactions", views.TransactionViewSet, basename="transaction")
+
+urlpatterns = router.urls
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from users.urls import router as users_router
+
+router = DefaultRouter()
+router.registry.extend(users_router.registry)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    
-    path('', include('users.urls')),
- #   path('', include('api.urls')),
+    path('', include(router.urls)),
+    path('', include('api.urls')),
 ]
