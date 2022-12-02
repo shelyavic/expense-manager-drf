2c4b4483f61d23a2f55aced75e0a506f3727f012
Ilya <ilya.shelepov2000@gmail.com>
2022-12-02 14:56:29 +0300

    Change Default Router to Simple Router

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from users import views
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register('users', views.UserViewSet, basename='user')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
