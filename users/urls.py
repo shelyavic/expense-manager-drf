2114a3bf41b90f1d0ecbf9fa323d1e433b0cba9d
Ilya <ilya.shelepov2000@gmail.com>
2022-12-03 16:57:44 +0300

    add swagger, remove DefaultRouter from main.urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from users import views
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-router = SimpleRouter()
-router.register('users', views.UserViewSet, basename='user')
+router = DefaultRouter()
+router.register("users", views.UserViewSet, basename="user")
 
-# urlpatterns = router.urls
+urlpatterns = router.urls
