00ad9a805163352bef241668aed7a2a42511f975
Ilya <ilya.shelepov2000@gmail.com>
2022-12-03 16:33:19 +0300

    add default categories for new users

--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
 from rest_framework.viewsets import ModelViewSet
 from users.serializers import UserSerializer
 from users.models import CustomUser
+from api.models import Category
+
+DEFAULT_CATEGORIES = [
+    "self care",
+    "salary",
+    "health and fitness",
+    "cafes and restaurants",
+    "car",
+    "education",
+    "recreation and entertainment",
+    "payments, commissions",
+    "purchases: clothes, appliances",
+    "groceries",
+    "travel",
+]
+
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
-    
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        for category_name in DEFAULT_CATEGORIES:
+            category, is_created = Category.objects.get_or_create(name=category_name)
+            category.users.add(user)
