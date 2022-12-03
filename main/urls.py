dd11db15ea7659555dc176b91edb23f8876f4932
Ilya <ilya.shelepov2000@gmail.com>
2022-12-03 16:21:53 +0300

    add JWT

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.urls import router as users_router
 
@@ -9,8 +13,10 @@ router.registry.extend(users_router.registry)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
-    path('', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include(router.urls)),
+    path("", include("api.urls")),
 ]
