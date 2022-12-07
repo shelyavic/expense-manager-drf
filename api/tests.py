4d71626251eceb9f554ed6bd070762cae2ce92f3
Ilya <ilya.shelepov2000@gmail.com>
2022-12-08 01:26:36 +0300

    simplify setUp

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework_simplejwt.tokens import AccessToken
 from api.models import Category
 
 
@@ -10,9 +11,9 @@ class CategoryTests(APITestCase):
     def setUpTestData(cls):
         cls.test_user_email = "test_user1@example.com"
         cls.test_user_password = "123456789"
-        test_user1 = get_user_model().objects.create(email="test_user1@example.com")
-        test_user1.set_password(cls.test_user_password)
-        test_user1.save()
+        cls.test_user1 = get_user_model().objects.create(email=cls.test_user_email)
+        cls.test_user1.set_password(cls.test_user_password)
+        cls.test_user1.save()
 
         cls.data_create = {
             "name": "category name",
@@ -23,14 +24,8 @@ class CategoryTests(APITestCase):
         }
 
     def setUp(self):
-        token_url = reverse("token_obtain_pair")
-        credentials = {
-            "email": self.test_user_email,
-            "password": self.test_user_password,
-        }
-        response = self.client.post(token_url, credentials, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + response.data["access"])
+        token = AccessToken.for_user(self.test_user1)
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(token))
 
     def test_category_create(self):
         url = reverse("api:category-list", kwargs={"user_pk": 1})
